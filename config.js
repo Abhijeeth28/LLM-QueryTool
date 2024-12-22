d42b2a32db1193dfc105c51b5be68484777734c0
Abhijeeth28 <65331036+Abhijeeth28@users.noreply.github.com>
2024-12-22 23:57:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+module.exports = {
+    llmProvider: 'openai',
+    llmApiKey: 'XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX',
+    llmApiEndpoint: 'https://api.openai.com/',
+    llmApiPath: '/v1/chat/completions',
+    llmModel: 'gpt-3.5-turbo',
+    port: 3000,
+};
